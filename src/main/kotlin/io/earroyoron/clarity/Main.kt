f3ac54bcf5e5ad03d302d921aa5af7d9cf17c087
Ernesto Arroyo <earroyoron@yahoo.es>
2020-02-22 10:31:09 +0100

    Separate the HostConnection data class in a different file for cleaner code

--- src/main/kotlin/io/earroyoron/clarity/Main.kt
+++ src/main/kotlin/io/earroyoron/clarity/Main.kt
@@ -8,11 +8,3 @@ fun validArguments (args: Array<String>): Boolean {
         return args.size == 4
 }
 
-data class HostConnection(val timestamp: Long,
-                          val origin: String,
-                          val target: String)
-
-fun String.toHostConnection(): HostConnection {
-    val elements: List<String> = this.split(" ")
-    return HostConnection(elements[0].toLong(), elements[1], elements[2])
-}
