7be49c0782631a63c3d50e60a4b5b77e798e1273
Ernesto Arroyo <earroyoron@yahoo.es>
2020-02-23 19:26:53 +0100

    Refactored

--- src/main/kotlin/io/earroyoron/clarity/Checks.kt
+++ src/main/kotlin/io/earroyoron/clarity/Checks.kt
@@ -2,27 +2,52 @@ package io.earroyoron.clarity
 
 import io.earroyoron.clarity.Checks.*
 
-
+/**
+ * This package contains utilities to validate and manage
+ * the command line parameters
+ */
 sealed class Checks {
     data class ParsingFileWithParameters(val filename: String,
                                          val fromTimestamp: Long,
                                          val toTimestamp: Long,
                                          val toHostname: String) : Checks()
-    object InvalidNumberOfArguments: Checks()
+    data class MonitoringFileWithParameters(val filename: String,
+                                            val fromHost: String,
+                                            val toHost: String) : Checks()
+    object InvalidArguments: Checks()
     object InvalidTimeFormat: Checks()
     object InvalidTimePeriod: Checks()
 }
 
+/**
+ * Checks arguments and return an object wrapping them to run the tool.
+ */
 fun Array<String>.checkProvidedArguments(): Checks {
-    return if (this.size != 4)
-        InvalidNumberOfArguments
-    else if ( null == this[1].toLongOrNull() || null == this[2].toLongOrNull()  )
+    return when {
+        ((this[0] == "--tail") && (this.size == 4)) -> this.getMonitoringParameters()
+        ((this[0] == "--parse") && (this.size == 5))-> this.getParsingParameters()
+        else -> InvalidArguments
+    }
+}
+
+/**
+ * Checks parameters when the tool is used with --parse option
+ */
+fun Array<String>.getParsingParameters(): Checks =
+    if ( null == this[2].toLongOrNull() || null == this[3].toLongOrNull()  )
         InvalidTimeFormat
-    else if ( this[1] > this[2] )
+    else if ( this[2] > this[3] )
         InvalidTimePeriod
     else ParsingFileWithParameters(
-        filename = this[0],
-        fromTimestamp = this[1].toLong(),
-        toTimestamp = this[2].toLong(),
-        toHostname = this[3])
-}
+        filename = this[1],
+        fromTimestamp = this[2].toLong(),
+        toTimestamp = this[3].toLong(),
+        toHostname = this[4])
+
+fun Array<String>.getMonitoringParameters(): Checks =
+    MonitoringFileWithParameters(
+        filename = this[1],
+        fromHost = this[2],
+        toHost = this[3]
+       )
+
