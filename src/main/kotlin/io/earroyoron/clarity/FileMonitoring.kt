95c87a5f0e6bb5229faa99a23a4280f973075893
Ernesto Arroyo <earroyoron@yahoo.es>
2020-02-25 07:19:35 +0100

    Refactoring FileMonitoring structure

--- src/main/kotlin/io/earroyoron/clarity/FileMonitoring.kt
+++ src/main/kotlin/io/earroyoron/clarity/FileMonitoring.kt
@@ -4,10 +4,7 @@ import io.earroyoron.clarity.Checks.MonitoringFileWithParameters
 import java.io.BufferedReader
 import java.io.File
 import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedTransferQueue
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TransferQueue
+import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
@@ -20,25 +17,57 @@ import kotlin.concurrent.withLock
  * A more complex solution could use the Reactive API
  * and some complete reactive implementation (RxJava,Reactor,...)
  */
-class FileMonitoring(private val arguments: MonitoringFileWithParameters) {
+class FileMonitoring(val arguments: MonitoringFileWithParameters) {
+
+    var toHostConnection = mutableListOf<String>()
+    var fromHostConnection = mutableListOf<String>()
+    var grouping = mutableMapOf<String, Int>()
+
+    val lock: Lock = ReentrantLock()
+    val linesRead = AtomicInteger() // Counting read lines, for curiosity
+
+    val transferQueue: TransferQueue<String> = LinkedTransferQueue()
+    private val executor: ExecutorService = Executors.newFixedThreadPool(2)
 
     fun start() {
-        val transferQueue: TransferQueue<String> = LinkedTransferQueue()
-        val executor = Executors.newFixedThreadPool(2)
+
         val producer = Producer(transferQueue, arguments.filename)
-        val consumer = Consumer(transferQueue, arguments.fromHost, arguments.toHost)
+        val consumer = Consumer(this)
+
+        executor.execute(producer)
+        executor.execute(consumer)
         timer(
             name = "Scheduler Metrics on Consumer",
             daemon = false,
-            initialDelay = 5000L,
+            initialDelay = 2500, //5secs to start
             period = 3600000L, //1h
-            action = { consumer.reporting() }
+            action = { reporting() }
         )
-        executor.execute(producer)
-        executor.execute(consumer)
         executor.awaitTermination(5000, TimeUnit.MICROSECONDS) //actually never
         executor.shutdown()
     }
+
+    /**
+     * Monitor and empty results, called by a scheduler timer from the
+     * main coordinator
+     */
+    fun reporting() {
+        println ("---REPORT--- (${linesRead.getAndSet(0)} lines processed)")
+        lock.withLock { // while printing we cannot update
+            print("1) Connections to $arguments.toHost: ")
+            println(toHostConnection.joinToString(","))
+            toHostConnection = mutableListOf()
+            print("2) Connections from $arguments.fromHost: ")
+            println(fromHostConnection.joinToString(","))
+            fromHostConnection = mutableListOf()
+            print("3) Most connected: ")
+            println(grouping.toList().maxBy { it.second })
+            grouping = mutableMapOf()
+            println("---END REPORT--- next report in 1h")
+            println()
+        }
+    }
+
 }
 
 /**
@@ -59,7 +88,7 @@ class Producer(
                 if (null != line) { //here we block for consumer with the TO of 500ms
                     transferQueue.tryTransfer(line, 500, TimeUnit.MILLISECONDS)
                 } else {
-                    Thread.sleep(5000) // producer sleeps for 5sec
+                    Thread.sleep(5000) // sleep some time awaiting for a new line...
                 }
             } catch (e: InterruptedException) {
                 e.printStackTrace()
@@ -72,51 +101,24 @@ class Producer(
  * Consumer doing calculations and printing report
  * with a Timer
  */
-class Consumer(private val transferQueue: TransferQueue<String>,
-               private val fromHost: String,
-               private val toHost: String) : TimerTask() {
-
-    private var toHostConnection = mutableListOf<String>()
-    private var fromHostConnection = mutableListOf<String>()
-    private var grouping = mutableMapOf<String, Int>()
-
-    private val lock: Lock = ReentrantLock()
-    private val linesRead = AtomicInteger() // Counting read lines, for curiosity
+class Consumer(private val fileMonitoring: FileMonitoring) : TimerTask() {
 
     override fun run() {
         while (true) {
-            val connection: HostConnection = transferQueue.take().toHostConnection()
-            linesRead.incrementAndGet() // Just for fun
-            lock.withLock { // while updating we cannot print
-                connection.apply {
-                    if (origin == fromHost) fromHostConnection.add(target)
-                    if (target == toHost) toHostConnection.add(origin)
-                    val actual = grouping.getOrDefault(origin, 0)
-                    grouping[origin] = actual + 1
-                }
+                val connection: HostConnection = fileMonitoring.transferQueue.take().toHostConnection()
+                fileMonitoring.linesRead.incrementAndGet() // Just for fun
+                fileMonitoring.lock.withLock {
+                    // while updating we cannot print
+                    connection.apply {
+                        if (origin == fileMonitoring.arguments.fromHost) fileMonitoring.fromHostConnection.add(target)
+                        if (target == fileMonitoring.arguments.toHost) fileMonitoring.toHostConnection.add(origin)
+                        val actual = fileMonitoring.grouping.getOrDefault(origin, 0)
+                        fileMonitoring.grouping[origin] = actual + 1
+                    }
             }
         }
     }
 
-    /**
-     * Monitor and empty results, called by a scheduler timer from the
-     * main coordinator
-     */
-    fun reporting() {
-        println ("---LAST HOUR REPORT--- (${linesRead.getAndSet(0)} lines processed)")
-        lock.withLock { // while printing we cannot update
-            print("1) Connections to $toHost: ")
-            println(toHostConnection.joinToString(","))
-            toHostConnection = mutableListOf()
-            print("2) Connections from $fromHost: ")
-            println(fromHostConnection.joinToString(","))
-            fromHostConnection = mutableListOf()
-            print("3) Most connected: ")
-            println(grouping.toList().maxBy { it.second })
-            grouping = mutableMapOf()
-            println("---END REPORT--- next report in 1h")
-            println()
-        }
-    }
+
 }
 
